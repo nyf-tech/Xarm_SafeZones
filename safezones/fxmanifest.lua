7dec6ed982dead8589a2623530b5778667ac010d
Xarmkox <67359464+xarmkox@users.noreply.github.com>
2022-01-06 21:27:28 +0100

    Add files via upload

---
+++ safezones/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+
+name 'SafeZones'
+author 'egzotic#4646&Xarm#9999'
+version '2.0'
+
+client_script {                                -- Client
+'main.lua',
+}
+
+
+
+
